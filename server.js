05bb0fb2cdde67fe6a2f60438e8940b34bacd54c
ggbandit <ggbandit@hotmail.com>
2018-08-19 17:07:32 +0700

    Node CRUD student-guide

--- server.js
+++ server.js
@@ -1,56 +1,95 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const mysql = require('mysql')
-const cors = require('cors')
 
 const app = express()
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "chanpreecha1!",
-  database: "student-guide"
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'chanpreecha1!',
+    database: 'student-guide'
 })
 
-connection.connect(err => {
-  if(err) {
-    return err
-  }
-})
-
-const SELECT_ALL_STUDENT_GUIDE = "SELECT * FROM `student-guide-list`"
-
-app.use(cors())
-
-app.get('/', (req,res) => {
-  res.send("go to /api/student-guide")
+mysqlConnection.connect((err) => {
+    if(err) {
+        console.log('DB connection error.')
+    } else {
+        console.log('DB connection successful.')
+    }
 })
 
 app.get('/api/student-guide', (req,res) => {
-  connection.query(SELECT_ALL_STUDENT_GUIDE, (err, results) => {
-    if(err) {
-      return err
-    } else {
-      res.json({
-        data: results
-      })
-    }
-  })
+    mysqlConnection.query('SELECT * FROM `student-guide-list`', (err, rows, fields) => {
+        if(!err) {
+            res.send(rows)
+        } else {
+            console.log(err)
+        }
+    })
 })
 
-app.get('/api/student-guide/add', (req,res) => {
-  const { name, year, languages, experience} = req.query
-  const INSERT_STUDENT_GUIDE = `INSERT INTO 'student-guide-list'(name, year, languages, experience) VALUES('${name}', '${year}', '${languages}', '${experience}')`
-  connection.query(INSERT_STUDENT_GUIDE, (err, results) => {
-    if(err) {
-      return err
-    } else {
-      return res.send("Insert student guide successful")
-    }
-  })
+app.get('/api/student-guide/:id', (req,res) => {
+    mysqlConnection.query('SELECT * FROM `student-guide-list` WHERE id = ?',[req.params.id], (err, rows, fields) => {
+        if(!err) {
+            res.send(rows)
+        } else {
+            console.log(err)
+        }
+    })
 })
+
+app.delete('/api/student-guide/:id', (req,res) => {
+    mysqlConnection.query('DELETE FROM `student-guide-list` WHERE id = ?',[req.params.id], (err, rows, fields) => {
+        if(!err) {
+            console.log('Deleted `student-guide-list` successful.')
+            res.send('Deleted `student-guide-list` successful.')
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+app.post('/api/student-guide', (req,res) => {
+    var value = {}
+      value.name = req.body.name
+      value.year = req.body.year
+      value.languages = req.body.languages
+      value.experience = req.body.experience
+    mysqlConnection.query("INSERT INTO `student-guide-list` SET name = ?, year = ?, languages = ?, experience = ?", [value.name,value.year,value.languages,value.experience], function(err, results, fields) {
+      if (err) throw err
+      console.log(req.body, 'success')
+      res.send(results)
+    })
+  })
+
+app.put('/api/student-guide/:id',function(req, res) {
+    var value = {}
+        value.id = req.params.id
+        value.name = req.body.name
+        value.year = req.body.year
+        value.languages = req.body.languages
+        value.experience = req.body.experience
+        console.log(value.id)
+      if(value.id)  
+      {
+            mysqlConnection.query("UPDATE `student-guide-list` SET name = ? , year = ?, languages = ?, experience = ? WHERE id = ?", [value.name, value.year, value.languages, value.experience, value.id],
+            function(err, results, fields) {
+            if (err) throw err
+            console.log("update id =", req.params.id, "success!")
+            res.send(results)
+        })
+      }
+      else {
+          throw err
+      }
+  })
 
 const port = 5000
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+    console.log(`Server listening on port ${port}`)
 })
